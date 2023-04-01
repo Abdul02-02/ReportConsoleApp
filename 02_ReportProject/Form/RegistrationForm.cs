01122824dd4aec7531897b73ce8f9d5e821f2e6a
Abdul02-02 <111751332+Abdul02-02@users.noreply.github.com>
2023-04-01 17:47:22 +0200

    ladda upp

---
+++ 02_ReportProject/Form/RegistrationForm.cs
@@ -0,0 +1,11 @@
+﻿namespace _02_ReportProject.Form;
+
+public class RegistrationForm
+{
+    public string FirstName { get; set; } = null!;
+    public string LasttName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public DateTime Time { get; set; }
+    public string Status { get; set; } = null!;
+}
